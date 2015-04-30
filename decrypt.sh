20cac340bc1d213f57597aa21e7e65193cb1bdc7
tecgen <>
2015-04-30 15:01:17 +0200

    improved error handling

--- decrypt.sh
+++ decrypt.sh
@@ -1,2 +1,44 @@
 #!/bin/bash
-openssl enc -d -aes-256-cbc -a -in $1 -out $2
+me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+argumentCount=$#
+password=$2
+
+# load configurations
+source user.cfg
+source global.cfg
+
+checkIfParametersAreGiven $me $argumentCount
+
+# get file name and extension
+filenameWithExtension=$(basename "$1")
+# persist extension only
+extension="${filenameWithExtension##*.}"
+filename="${filenameWithExtension%.*}"
+
+#debug
+#echo in : $filenameWithExtension
+#echo out: $filename
+
+# when one parameter is available
+if [ $argumentCount -eq 1 ]
+then
+  openssl enc -d -$cipher -a -in $filenameWithExtension -out $filename
+fi
+
+# when two parameters are available
+if [ $argumentCount -eq 2 ]
+then
+  openssl enc -d -$cipher -a -in $filenameWithExtension -out $filename -k $password
+fi
+
+
+# when error code is 0
+if [ $? -eq 0 ]
+  then
+  echo "$filename => decryped"
+  exit 0
+else
+  echo "$filename => NOT decrypted" >&2
+  # return error code 1 to caller
+  exit 1
+fi
--- encrypt.sh
+++ encrypt.sh
@@ -1,7 +1,41 @@
 #!/bin/bash
+me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+argumentCount=$#
+password=$2
+
+# load configurations
+source user.cfg
+source global.cfg
+
+
+checkIfParametersAreGiven $me $argumentCount
+
+# get file name and extension
 filenameWithExtension=$(basename "$1")
+# persist extension only
 extension="${filenameWithExtension##*.}"
 filename="${filenameWithExtension%.*}"
-openssl enc -aes-256-cbc -a -salt -in $filenameWithExtension -out $filename.$extension.encrypted
-rm $filenameWithExtension
 
+# when one parameter is available
+if [ $argumentCount -eq 1 ]
+then
+  openssl enc -$cipher -a -salt -in $filenameWithExtension -out $filenameWithExtension.$encrypted
+fi
+
+# when two parameters are available
+if [ $argumentCount -eq 2 ]
+then
+  openssl enc -$cipher -a -salt -in $filenameWithExtension -out $filenameWithExtension.$encrypted -k $password
+fi
+
+# when error code is 0
+if [ $? -eq 0 ]
+then
+  echo "$filename => encryped"
+  exit 0
+else
+  echo "$filename => NOT encrypted" >&2
+  # return error code 1 to caller
+  exit 1
+  #rm $filenameWithExtension
+fi
